a086e3b5e34591bacf2f6cfb9a324c2a4dcc7056
Juanbermudezp <00218123@uca.edu.sv>
2024-12-02 17:17:41 -0600

    :memo: Modificacion

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.use('/api/matches', matchRoutes);
 app.use('/api/geocode', geocodeRoutes);
 
 // Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
